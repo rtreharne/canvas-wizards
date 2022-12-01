202379535e6b7a1d16a71023541e0183260ea5bc
rtreharne <R.Treharne@liverpool.ac.uk>
2022-12-01 09:19:08 +0000

    auto update from .deploy.sh

---
+++ book/_build/jupyter_execute/getting_started.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Getting Started
+# 
+# ## What is an API?
+# 
+# An **application programming interface** (API) is a set of stored functions or procedures that allow you get, create or edit data from an application without having to use a graphical interface (e.g. a website). The Canvas API will allow you to do almost anything that you currently do using the standard user interface. 
+# 
+# The Canvas API is extremely well documented and, while the documentation may seem somewhat impenetrable at first, reading the docs can solve most problems. Check it out at <a href="https://canvas.instructure.com/doc/api/" target="_blank">https://canvas.instructure.com/doc/api/</a>.
+# 
+# *"So what?"* - I hear you say. Well, the power of the API lies with the ability to prepare snippets of code (we're going to use **Python** here) to make use of the API's functions in order to automate repetitive tasks and cut your workload dramatically.
+# 
+# *"OMG, I have to write code?"*. Well, yes. Get over it. It's really not that bad and this book is going to guide you all the way. Deep breath now, let's dive in.
+
+# ## Setup and Configuration
+# 
+# There are about a **zillion** different ways that you could create a coding environment in which you can begin your tranformation to Canvas wizard. The best way for you, to begin with, will be to click the little *rocket icon* at the top of this page and then click the *Binder* button.
+
+# In[ ]:
+
+
+
+
