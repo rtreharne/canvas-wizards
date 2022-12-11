3270c62ec0e46d36aac596d90fecc4c7b3937e4c
rtreharne <rtreharne@github.com>
2022-12-11 18:02:11 +0000

    update from raspberry pi

--- book/_build/jupyter_execute/essentials.py
+++ book/_build/jupyter_execute/essentials.py
@@ -12,7 +12,7 @@
 # 
 # Before we begin, create your `canvas` session variable again by running the cell below with `API_URL` AND `API_KEY` set to your own personal values.
 
-# In[1]:
+# In[45]:
 
 
 # Create your Canvas session
@@ -22,8 +22,11 @@ from canvasapi import Canvas
 API_URL = "" # e.g. "https://liverpool.instructure.com"
 API_KEY = ""
 
+API_URL = "<YOUR_CANVAS_URL>" # Put your canvas URL here, e.g. "https://canvas.liverpool.ac.uk"
+API_KEY = "<YOUR_API_KEY>"
+
 API_URL = "https://canvas.liverpool.ac.uk"
-API_KEY = "15502~DqhNjK9tqamH68fsXvkoPkvH3Jgk5Tu5Uf5p4MFiCjoiUKGJBeqR7vgFs3yOj9Oe"
+API_KEY = "15502~AucX1kPx4A7c0hsJf4Z5JnpgLL0INvjLRGQK98jcy7sR53dBauGKqmAS01KU6J6W"
 
 canvas = Canvas(API_URL, API_KEY)
 
@@ -38,7 +41,7 @@ canvas = Canvas(API_URL, API_KEY)
 # If you don't know a course's id, you can get it by going to the course's homepage on Canvas and then looking at the last number in the URL string, e.g. for https://canvas.liverpool.ac.uk/courses/60371 the course id is 60371.
 # ```
 
-# In[ ]:
+# In[46]:
 
 
 course_id = 61073 # REPLACE 61073 WITH YOUR COURSE ID - I'm just giving an example here.
@@ -47,7 +50,7 @@ course = canvas.get_course(course_id)
 
 # To view all of the settings for the course use the `__dict__` method:
 
-# In[ ]:
+# In[47]:
 
 
 course.__dict__
@@ -55,7 +58,7 @@ course.__dict__
 
 # If you want, you can also get a course based on the course_code which is usually the course name as it appears in the top left of the course homepage on Canvas. You will need to use the additional parameter `use_sis_id` in the `get_course()` function, and make sure it's set to a value of `True`.
 
-# In[ ]:
+# In[51]:
 
 
 # Getting a course with its course_code instead of the id
@@ -66,7 +69,7 @@ course = canvas.get_course("LIFE733-202223", use_sis_id=True)
 
 # Use the `get_enrollments()` function to get all the enrollments associated with the course.
 
-# In[ ]:
+# In[52]:
 
 
 # To get all the enrollments on a course
@@ -75,7 +78,7 @@ enrollments = course.get_enrollments()
 
 # The line above will successfully return all enrollments for a course and store them in the variable `enrollments`. However, all the information will stored in an object called a `PaginatedList` which isn't that useful when trying to do stuff in Python. For example, if you want to figure out how many enrollments there are you might try and use the `len()` function (very sensible), but for a `PaginatedList` you'd encounter a `TypeError`.
 
-# In[ ]:
+# In[53]:
 
 
 len(enrollments)
@@ -83,7 +86,7 @@ len(enrollments)
 
 # To avoid `TypeErrors`, it is recommended that you retrieve list of things from canvas by using the relevant function, in this case `course.get_enrollments()`, within what is known as a "list comprehension" in Python. Without going into any futher detail, the code you need is as follows ...
 
-# In[ ]:
+# In[54]:
 
 
 enrollments = [x for x in course.get_enrollments()]
@@ -93,7 +96,7 @@ enrollments = [x for x in course.get_enrollments()]
 # This slightly more complicated way of obtaining a list of things from Canvas is better in the long run. You'll see it used a lot when I'm getting enrollments, assignments and submissions.
 # ```
 
-# In[ ]:
+# In[55]:
 
 
 len(enrollments) # That's better! I can see there are 68 enrollments on this course.
@@ -101,7 +104,7 @@ len(enrollments) # That's better! I can see there are 68 enrollments on this cou
 
 # Also, you can now slice your list of enrollments, e.g. to pick out a single enrollment from the list to look at it, by doing something like this ...
 
-# In[ ]:
+# In[56]:
 
 
 # Example: Pick out the 65th enrollment from a list of enrollments.
@@ -110,7 +113,7 @@ enrollments[65]
 
 # To make the data associated with the enrollment a bit more readable you can use the `__dict__` method.
 
-# In[ ]:
+# In[57]:
 
 
 # Use .__dict__ to print out the dictionary map for the enrollment object. Beatuful!
@@ -119,7 +122,7 @@ enrollments[65].__dict__
 
 # You can access any "key value" associated with the enrollment object by using `.<key_name>` on any enrollment object. For example, to get the role associated the object above:
 
-# In[ ]:
+# In[58]:
 
 
 # Get the role associated with my enrollment
@@ -128,7 +131,7 @@ enrollments[65].role
 
 # Sometimes, you just want to get the enrollments associated with a particular role, e.g. ``StudentEnrolment``. To do this you can filter accordingly using the following line.
 
-# In[ ]:
+# In[59]:
 
 
 student_enrollments = [x for x in course.get_enrollments() if x.role == "StudentEnrollment"]
@@ -142,7 +145,7 @@ student_enrollments = [x for x in course.get_enrollments() if x.role == "Student
 # 
 # Run the cell below to create a Pandas data frame containing your enrollment data.
 
-# In[ ]:
+# In[60]:
 
 
 # The pandas module is fantastic for reading/creating spreadsheets from data in Python
@@ -166,7 +169,7 @@ output = pd.DataFrame(rows) # Convert your rows into a Pandas dataframe
 
 # You can view the first 5 rows of your data frame using the following command.
 
-# In[ ]:
+# In[61]:
 
 
 # check it out!
@@ -175,7 +178,7 @@ output.head()
 
 # Finally, lets save the data frame to something we all know and love - the humble Excel spreadsheet.
 
-# In[ ]:
+# In[62]:
 
 
 # Save the output to an Excel spreadsheet
@@ -198,7 +201,7 @@ output.to_excel("course_enrollments.xlsx", index=False)
 
 # Below is an example of how to enroll a new user (in this case me! - my id is a very Orwellian 101) as a Teacher on a course. The `enrollment` dictionary contains information on the `enrollment_state` of the user. By setting it to `active` we can avoid sending the user any notification of their enrollment which is sometimes preferable.
 
-# In[ ]:
+# In[63]:
 
 
 # Enrollment example
@@ -215,20 +218,20 @@ course.enroll_user(user=101, #the id of the user
 # 
 # Very often it is necessary to take all the enrollments from one course and replicated them on another. Remember that `course_enrollments.xlsx` spreadsheet that you created above. Let's use this to enrol all of the students on another course.
 
-# In[ ]:
+# In[64]:
 
 
 # Read the spreadsheet to a data frame variable called `user_sheet`
 user_sheet = pd.read_excel("course_enrollments.xlsx")
 
 
-# In[ ]:
+# In[65]:
 
 
 user_sheet.columns
 
 
-# In[ ]:
+# In[66]:
 
 
 # enroll them on another course
@@ -264,7 +267,7 @@ for i, row in user_sheet.iterrows():
 # 
 # As an example, let's get all the student enrollments on a course, print them out in a big list and then delete the enrollment chose enrollment.
 
-# In[ ]:
+# In[67]:
 
 
 # Get all student enrollments on the test_course we used above
@@ -273,7 +276,7 @@ for i, row in user_sheet.iterrows():
 enrollments = [x for x in test_course.get_enrollments() if x.type=="StudentEnrollment" and "Treharne" not in x.user["name"]]
 
 
-# In[ ]:
+# In[68]:
 
 
 # Loop over all your enrollments and print out the index and user name
@@ -283,18 +286,18 @@ for i, e in enumerate(enrollments):
 
 # Select the enrollmet you want to delete by using `[x]` at the end of your `enrollments` variable, where `x` is the index of the enrollment (you printed them out above).
 
-# In[ ]:
+# In[69]:
 
 
 # Select the enrollment to delete
 # Arbitrarily I'm going to delete the first enrollment (i.e. use [0] to slice)
 
-enrollment_to_delete = enrollments[15]
+enrollment_to_delete = enrollments[0]
 
 
 # Now, use the `.deactivate` function. Make sure you pass it a `task` variable with the value `"delete"` set.
 
-# In[ ]:
+# In[70]:
 
 
 # Select the enrollment you want to delete by using `[x]` at the end of your `enrollment
@@ -308,7 +311,7 @@ enrollment_to_delete.deactivate(task="delete")
 # There's no way of undoing the following. It is strongly recommended that you export the course enrollments to a spreadsheet (see above) and keep the file in a safe place just in case you need to re-add the students to the course.
 # ```
 
-# In[ ]:
+# In[71]:
 
 
 # Remove ALL students from a course
@@ -324,29 +327,25 @@ for e in enrollments:
 
 # ## Assignments
 # 
-# TODO:
+# I'd say that we (Jack and I) mostly use the Canvas API to create, edit and update assignments. Our School has over 750 assignments per year(not including resit assignments), spread accross 150 separate Canvas courses. Trying to manage these assignments without the Canvas API is a grinding, repetitive task (trust us on this!). Instead, the API lets us automate the creation of our assignments every September so that we can get on with more interesting projects. Hooray!
 # 
-# + Getting Assignments
-# + Editing Existing Assignments
-# + Creating New assignments
-# + Duplicating assignments
+# What follows is a description of our most commonly used API actions associated with assignments.
 
-# In[ ]:
-
-
-### Getting Assignments
-
-
-
-# In[ ]:
+# In[1]:
 
 
 from canvasapi import Canvas
+canvas = Canvas(
+    "https://canvas.liverpool.ac.uk",
+    "15502~0h0tC35okrJhOw0KPEX7V4cvrfeU0PiztrZ5mNAxfd6iCqK3tGd31fjjBTAmnbFQ"
+)
 
-canvas = Canvas("https://canvas.liverpool.ac.uk", "15502~DqhNjK9tqamH68fsXvkoPkvH3Jgk5Tu5Uf5p4MFiCjoiUKGJBeqR7vgFs3yOj9Oe")
 
+# ### Getting Assignments
+# 
+# In this, and the following sections I use my own personal test course in Canvas to showcase manipulating assignments with the Canvas API. Note that you will only be able to access courses via the API that you are enrolled on as a Teacher or for those that admin privelidges have been granted.
 
-# In[ ]:
+# In[72]:
 
 
 # first get the course
@@ -356,63 +355,101 @@ course = canvas.get_course(53523) # 53523 is the id of my personal test course
 assignment = course.get_assignment(224200) # 224200 is the assignment id. Get it from the assignment url. 
 
 
-# In[ ]:
+# The object that you now have stored in the `assignment` variable will tell you pretty much everything you ever needed to know about an assignment's settings. Let's take a look via the [`__dict__`](https://docs.python.org/3/library/stdtypes.html#object.__dict__) method.
+
+# In[73]:
 
 
 # inspect the assignment object
 assignment.__dict__
 
 
-# In[ ]:
+# Sometimes, you don't know the exact id of the assignment that you want but can't be bothered to navigate to a course's assignments page. To get a list of *all* the assignment objects associated with a course you can use the following line.
+
+# In[74]:
 
 
 # get all course assignments
-assignments = [x for x in course.get_assignments()]
+assignments = [x for x in course.get_assignments()] # This particular syntax is known as a list comprehension in Python.
 
-
-# In[ ]:
-
-
+# print out all the assignments and their clickable urls!
 for a in assignments:
-    print(a)
+    print(a, a.html_url)
 
 
-# In[ ]:
+# You can tweak the list comprehension above to filter assignments based on it's properties. For example:
+
+# In[75]:
 
 
-# filter assignments based on key value
-assignments = [x for x in course.get_assignments() if x.due_at == "2023-01-09T12:00:00Z"]
+# filter assignments based on a due date
+assignments = [x for x in course.get_assignments() if x.due_at == "2023-01-09T16:00:00Z"]
 
 
-# In[ ]:
+# In[76]:
 
 
-assignments = [x for x in course.get_assignments() if "test" in x.name.lower()]
+# or, here's another example where I search for a substring in an assignmet's name
+assignments = [x for x in course.get_assignments() if "awesome" in x.name.lower()]
 
 
 # ### Edit Assignments
 
-# In[ ]:
+# Once you've got an assignment object you edit it using the API as follows:
+
+# In[78]:
 
 
-a = assignments[0] # from above
+a = assignments[0] # slicing my assignments variable from above to get the assignment
 
+# create a dictionary of keys and values that you want to update
 assignment_update_info = {
     "description": "Important information about this assignment",
     "due_at": "2023-01-09T16:00:00Z", # important to get the format correct
-    "name": "Awesome Assignment",
-    "published": False
+    "name": "Summative Test",
+    "published": False # Students won't be able to see this assignment
 }
 
-a = a.edit(assignment=assignment_update_info)
+a = a.edit(assignment=assignment_update_info) # This line makes the changes in Canvas
+
+
+# ### Create New Assignments
+# Creating new assignments with the the API is straightforward. Do it like this
+
+# In[79]:
+
+
+new_assignment = course.create_assignment(assignment={
+        "name": "Assignment created with API",
+        "open_at": "2023-12-13T09:00:00Z",
+        "due_at": "2023-12-13T16:00:00Z",
+        "submission_types": ["online_upload"],
+        "anonymous_grading": True,
+        "published": True
+    }
+)
+print(new_assignment.html_url)
 
 
 # ### Duplicate Assignments
+# 
+# The one thing you **CAN'T** do with the API is configure third party tools associated with an assignment, for example a Turnitin Plagiarism Review Tool. This is mildly frustrating but there is a workaround - you can duplicate any existing assignment that already has a third party tool configured and all the settings will be copied accross for your duplicated assignment.
 
-# In[ ]:
+# ```{note}
+# When duplicating assignments, any associated rubrics are also copied and attached to the new assignments
+# 
+# ```
+
+# There is no `duplicate_assigment()` function in the `canvasapi` module sadly. We have to do it the old fashioned way be defining a new function that makes direct use of the API's [duplicate](https://canvas.instructure.com/doc/api/assignments.html#method.assignments_api.duplicate) url endpoint.
+
+# In[83]:
 
 
+# run this cell to create your own duplicate_assignment function
 import requests
+
+API_URL = "https://canvas.liverpool.ac.uk"
+API_KEY = "15502~AucX1kPx4A7c0hsJf4Z5JnpgLL0INvjLRGQK98jcy7sR53dBauGKqmAS01KU6J6W"
 
 def duplicate_assignment(course_id, assignment_id):
     """
@@ -436,22 +473,18 @@ def duplicate_assignment(course_id, assignment_id):
     return assignment
 
 
-# In[ ]:
+# In[84]:
 
 
 # Running this cell will copy your assignment from above. Check this in Canvas
 new_assignment = duplicate_assignment(course.id, a.id)
+print(new_assignment.html_url)
 
 
-# In[ ]:
+# In[85]:
 
 
-new_assignment.name
-
-
-# In[ ]:
-
-
+# now you can proceed to edit the duplicated assignment
 new_assignment = new_assignment.edit(
     assignment={
         "name": "Another Awesome Assignment",
@@ -461,7 +494,9 @@ new_assignment = new_assignment.edit(
 )
 
 
-# In[ ]:
+# You can create as many duplicate assignments as you want in one go by using a loop. For example, let's create 5 duplicates and name then accordingly.
+
+# In[86]:
 
 
 for i in range(1,6):
